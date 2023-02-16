cc9e2f0bf7275435ddf8b1f297ea721bb9a44611
Vladyslav Soloviov <vladyslav.soloviov@photobox.com>
2023-02-16 21:35:57 +0200

    small fixes

--- courses-app/src/app/shared/pipes/duration.pipe.ts
+++ courses-app/src/app/shared/pipes/duration.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
  
 @Pipe({
-    name: 'duration'
+  name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
   transform(duration: number, args?: any): string {
